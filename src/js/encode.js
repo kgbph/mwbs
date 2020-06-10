24c4009c2b093ef430b4c6e252d2b573547b3498
Kevin Bernardo <kgb.acct.personal@gmail.com>
2020-06-10 16:39:35 +0800

    Implement linter. Upgrade drone CI/CD.

--- src/js/encode.js
+++ src/js/encode.js
@@ -1,94 +1,89 @@
-$(document).ready(function () {
+$(document).ready(() => {
+  $.ajax({
+    url: 'http://mngtool.ynsdev.pw/users/productivity',
+    method: 'GET',
+    context: this,
+    success(response) {
+      if ($(response).find('.productivity__wrapper').length) {
+        $('#frmEncode').prepend($('<div>', {
+          class: 'alert alert-danger',
+          text: 'Your tasks are already encoded. Please use the task management system for updating your tasks.',
+          role: 'alert',
+        }));
 
-    $.ajax({
-        url: 'http://mngtool.ynsdev.pw/users/productivity',
-        method: 'GET',
-        context: this,
-        success: function (response) {
-            if ($(response).find('.productivity__wrapper').length) {
-                $('#frmEncode').prepend($('<div>', {
-                    class: 'alert alert-danger',
-                    text: 'Your tasks are already encoded. Please use the task management system for updating your tasks.',
-                    role: 'alert'
-                }));
+        $('#frmEncode :input').prop('disabled', true);
+      }
+    },
+  });
 
-                $('#frmEncode :input').prop('disabled', true);
-            }
-        }
-    });
+  const taskRowTemplate = $('#tmpTaskRow').html();
 
-    let taskRowTemplate = $('#tmpTaskRow').html();
+  $('#tblTasks > tbody').append(taskRowTemplate);
 
+  $('#tblTasks').on('click', '.btn-add-task', () => {
     $('#tblTasks > tbody').append(taskRowTemplate);
 
-    $('#tblTasks').on('click', '.btn-add-task', function () {
-        $('#tblTasks > tbody').append(taskRowTemplate);
+    $(this)
+      .removeClass('btn-secondary btn-add-task')
+      .addClass('btn-danger btn-remove-task')
+      .text('Remove');
+  });
 
-        $(this)
-            .removeClass('btn-secondary btn-add-task')
-            .addClass('btn-danger btn-remove-task')
-            .text('Remove');
+  $('#tblTasks').on('click', '.btn-remove-task', () => {
+    $(this)
+      .parents('tr')
+      .remove();
+  });
+
+  $('#tblTasks').on('blur', '.inp-code', () => {
+    $.ajax({
+      url: 'http://mngtool.ynsdev.pw/users/find_task',
+      method: 'POST',
+      context: this,
+      data: { task_code: $(this).val() },
+      success(response) {
+        const parent = $(this)
+          .parents('td')
+          .addClass('was-validated');
+        parent
+          .find('.valid-feedback')
+          .text(response.data.task.name);
+        parent
+          .find('.inp-milestone')
+          .val(response.data.milestone.id);
+        parent
+          .find('.inp-project')
+          .val(response.data.project.id);
+      },
     });
+  });
 
-    $('#tblTasks').on('click', '.btn-remove-task', function () {
-        $(this)
-            .parents('tr')
-            .remove();
+  $('#frmEncode').submit((e) => {
+    e.preventDefault();
+
+    $('.alert').alert('close');
+
+    const tasks = $('#tblTasks > tbody > tr').map(() => ({
+      code: $(this).find('.inp-code').val(),
+      time: $(this).find('.inp-time').val(),
+      milestone: $(this).find('.inp-milestone').val(),
+      project: $(this).find('.inp-project').val(),
+    }));
+
+    $.when(...tasks.map(() => $.ajax({
+      url: 'http://mngtool.ynsdev.pw/users/productivity_add',
+      method: 'POST',
+      context: this,
+      data: {
+        work_date: new Date().toISOString().slice(0, 10),
+        task_code: this.code,
+        actual_times: this.time,
+        estimate_times: 0,
+        milestone_id: this.milestone,
+        project_id: this.project,
+      },
+    }))).done(() => {
+      window.location.href = 'done.html';
     });
-
-    $('#tblTasks').on('blur', '.inp-code', function () {
-        $.ajax({
-            url: 'http://mngtool.ynsdev.pw/users/find_task',
-            method: 'POST',
-            context: this,
-            data: { task_code: $(this).val() },
-            success: function (response) {
-                let parent = $(this)
-                    .parents('td')
-                    .addClass('was-validated');
-                parent
-                    .find('.valid-feedback')
-                    .text(response.data.task.name);
-                parent
-                    .find('.inp-milestone')
-                    .val(response.data.milestone.id);
-                parent
-                    .find('.inp-project')
-                    .val(response.data.project.id);
-            }
-        });
-    });
-
-    $('#frmEncode').submit(function (e) {
-        e.preventDefault();
-
-        $('.alert').alert('close');
-
-        let tasks = $('#tblTasks > tbody > tr').map(function () {
-            return {
-                code: $(this).find('.inp-code').val(),
-                time: $(this).find('.inp-time').val(),
-                milestone: $(this).find('.inp-milestone').val(),
-                project: $(this).find('.inp-project').val()
-            };
-        });
-
-        $.when(...tasks.map(function () {
-            return $.ajax({
-                url: 'http://mngtool.ynsdev.pw/users/productivity_add',
-                method: 'POST',
-                context: this,
-                data: {
-                    work_date: new Date().toISOString().slice(0, 10),
-                    task_code: this.code,
-                    actual_times: this.time,
-                    estimate_times: 0,
-                    milestone_id: this.milestone,
-                    project_id: this.project
-                }
-            });
-        })).done(function () {
-            location.href = 'done.html';
-        });
-    });
+  });
 });
--- src/js/login.js
+++ src/js/login.js
@@ -1,32 +1,32 @@
-var xhr;
-var _orgAjax = jQuery.ajaxSettings.xhr;
-jQuery.ajaxSettings.xhr = function () {
-    xhr = _orgAjax();
-    return xhr;
+let xhr;
+const orgAjax = jQuery.ajaxSettings.xhr;
+jQuery.ajaxSettings.xhr = () => {
+  xhr = orgAjax();
+  return xhr;
 };
 
-$(document).ready(function () {
-    $('#frmLogin').submit(function (e) {
-        e.preventDefault();
+$(document).ready(() => {
+  $('#frmLogin').submit((e) => {
+    e.preventDefault();
 
-        $('.alert').alert('close');
+    $('.alert').alert('close');
 
-        $.ajax({
-            url: 'http://mngtool.ynsdev.pw',
-            method: 'POST',
-            context: this,
-            data: $(this).serialize(),
-            complete: function () {
-                if (xhr.responseURL === 'http://mngtool.ynsdev.pw/current_project_status') {
-                    location.href = 'encode.html';
-                } else {
-                    $(this).prepend($('<div>', {
-                        class: 'alert alert-danger',
-                        text: 'Incorrect user name or password.',
-                        role: 'alert'
-                    }));
-                }
-            }
-        });
+    $.ajax({
+      url: 'http://mngtool.ynsdev.pw',
+      method: 'POST',
+      context: this,
+      data: $(this).serialize(),
+      complete() {
+        if (xhr.responseURL === 'http://mngtool.ynsdev.pw/current_project_status') {
+          window.location.href = 'encode.html';
+        } else {
+          $(this).prepend($('<div>', {
+            class: 'alert alert-danger',
+            text: 'Incorrect user name or password.',
+            role: 'alert',
+          }));
+        }
+      },
     });
+  });
 });
